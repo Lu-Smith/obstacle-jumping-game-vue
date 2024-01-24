9bcfc6d4799cc9c889e7858ce64dfa37d0e55819
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-24 22:36:26 +0000

    Initial folders

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import IntroPage from './components/IntroPage.vue'
+</script>
+
+<template>
+  <div>
+   Hello
+  </div>
+  <IntroPage msg="Welcome" />
+</template>
+
+<style scoped>
+
+</style>
