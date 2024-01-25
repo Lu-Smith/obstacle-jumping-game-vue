3fe2c7a100e6441f459a3f495e39be83b39a6cd0
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-25 16:38:58 +0000

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
-    <Game />
-    <IntroPage msg="Welcome" />
+    <div v-if="startGame">
+      <Game />
+    </div>
+    <div v-if="!startGame">
+      <IntroPage msg="Welcome" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import Game from './components/Game.vue';
 import IntroPage from './components/IntroPage.vue';
+import { ref } from 'vue';
+
+const startGame = ref(false);
 </script>
 
 <style lang="scss">
