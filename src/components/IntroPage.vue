f4ef89318505d6920713cf0644f9700fd46569f5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-25 18:47:31 +0000

    Updated IntroPage.vue styling

--- src/components/IntroPage.vue
+++ src/components/IntroPage.vue
@@ -19,5 +19,19 @@ const handleStartGame = () => {
   div {
     background: $primary-background;
     height: 30vw;
+
+    h1 {
+      color: $secondary-color;
+      font-size: 72px;
+    }
+
+    button {
+      padding: 10px 15px;
+      font-size: 18px;
+      border-radius: 5px;
+      background: $secondary-color;
+      color: $primary-background;
+      border: 3px solid $third-color;
+    }
   }
 </style>
