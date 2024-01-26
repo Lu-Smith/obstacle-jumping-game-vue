030adbf501140181ed4de15bdf4b27b2176a5090
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-26 12:11:44 +0000

    Updated Game.vue

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,6 +1,11 @@
 <template>
-    <div class="canvasContainer">
-        <canvas ref="gameCanvas"></canvas>
+    <div class="gameContainer">
+        <div class="buttonContainer">
+            <button @click="startGame">Go</button>
+        </div>
+        <div class="canvasContainer">
+            <canvas ref="gameCanvas"></canvas>
+        </div>
     </div>
 </template>
 
@@ -9,6 +14,8 @@
 
     const gameCanvas = ref<HTMLCanvasElement | null>(null);
     const direction = ref([ 1, 0 ]);
+    let gameInterval: undefined | number;
+    let intervalTime: number = 30;
 
     //game logic
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -41,6 +48,14 @@
         }
     };
 
+    const updateGame = () => {
+
+    }
+
+    const startGame = () => {
+        gameInterval = setInterval(updateGame, intervalTime);
+    }
+
     onMounted(() => {
         drawGame();
         window.addEventListener('keydown', handleKeyDown);
@@ -48,23 +63,44 @@
 </script>
 
 <style lang="scss">
-    .canvasContainer {
-        background: $secondary-background;
-        @include flex(center, center, column);
+    .gameContainer {
         height: 100vh;
         width: 100vw;
+        @include flex(center, center, column);
+    };
+
+    .buttonContainer {
+        flex: 1;
+        background: $secondary-background;
+        @include flex(center, center, column);
+
+        button {
+            padding: 10px 25px;
+            font-size: 22px;
+            border-radius: 10px;
+            background: $secondary-color;
+            color: $primary-background;
+            border: 3px solid $third-color;
+            box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.5);
+        }
     }
+
+    .canvasContainer {
+        flex: 3;
+        background: $secondary-background;
+        @include flex(center, start, column);
+    };
 
     canvas {
         background: $primary-background;
         width: 800px;
         height: 400px;
-    }
+    };
 
     @include mobile {
         canvas {
             width: 100%;
             height: auto;
         }
-    }
+    };
 </style>
