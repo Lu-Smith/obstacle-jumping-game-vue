d72410603154d9ed5479ae5873b85c5ac3f95224
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-04 14:21:27 +0000

    Updated player.ts

--- src/assets/player.ts
+++ src/assets/player.ts
@@ -11,8 +11,8 @@ export default class Player {
     constructor(game: Game) {
         this.game =  game;
         this.x = this.game.canvas.width;
-        this.y = this.game.canvas.height -22;
-        this.radius = 10;
+        this.y = this.game.canvas.height - 84;
+        this.radius = 24;
         this.color = '#5b086b';
         this.gameCanvasHeight =this.game.canvas.height;
         this.game =  game;
@@ -26,29 +26,30 @@ export default class Player {
     }
     
     updatePosition(jump: boolean, doubleJump: boolean) {
+        console.log(jump);
         // Update Y position
         if (jump) {
             this.y -= 2;
-            if (this.y < 90) {
-                this.y = 89;
-                setInterval(() => {
-                    this.y += 2;
-                    if (this.y >= 130) {
-                        this.y = 130;
-                    }
-                }, 100);
-            }  
+        //     if (this.y < 90) {
+        //         this.y = 89;
+        //         setInterval(() => {
+        //             this.y += 2;
+        //             if (this.y >= 130) {
+        //                 this.y = 130;
+        //             }
+        //         }, 100);
+        //     }  
         } else if (doubleJump) {
             this.y -= 3;
-            if (this.y < 70) {
-                this.y = 69;
-                setInterval(() => {
-                    this.y += 1.7;
-                    if (this.y >= 130) {
-                        this.y = 130;
-                    }
-                }, 100);
-            }
+        //     if (this.y < 70) {
+        //         this.y = 69;
+        //         setInterval(() => {
+        //             this.y += 1.7;
+        //             if (this.y >= 130) {
+        //                 this.y = 130;
+        //             }
+        //         }, 100);
+        //     }
         }
     }
 }
