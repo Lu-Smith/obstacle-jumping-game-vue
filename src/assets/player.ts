862a69919b5e152c794deb0afdbcf6aa25442614
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-04 18:09:10 +0000

    Updated player.ts

--- src/assets/player.ts
+++ src/assets/player.ts
@@ -10,18 +10,20 @@ export default class Player {
     
     constructor(game: Game) {
         this.game =  game;
-        this.x = this.game.canvas.width/2;
-        this.y = this.game.canvas.width - 84;
+        this.x = this.game.canvas.width;
         this.radius = 24;
+        this.y = this.game.canvas.height - this.game.floorHeight - this.radius;
         this.color = '#5b086b';
         this.gameCanvasHeight =this.game.canvas.height;
         this.game =  game;
     }
     
     draw(context: CanvasRenderingContext2D) {
+        const groundLevel = this.game.canvas.height - this.game.floorHeight - this.radius; 
+    
         context.fillStyle = this.color;
         context.beginPath();
-        this.game.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        this.game.context.arc(this.x, groundLevel, this.radius, 0, Math.PI * 2);
         context.fill();
         console.log(this.game.floorHeight);
     }
@@ -55,8 +57,6 @@ export default class Player {
     }
 
     resize() {
-        this.x = this.x * this.game.ratio;
-        this.y = this.y * this.game.ratio;
-        this.radius = this.radius * this.game.ratio;
+        this.radius = this.radius;
     }
 }
