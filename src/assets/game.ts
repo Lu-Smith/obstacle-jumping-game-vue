fb8f7fc7d10c40a44836c671d49ab9a5853ec587
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-04 18:08:42 +0000

    Updated game.ts

--- src/assets/game.ts
+++ src/assets/game.ts
@@ -37,22 +37,26 @@ export default class Game {
             }
         })
     }
-    draw() {
-    // Draw ground
-
-    this.context.fillStyle = '#f5f9ee';
-    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height-this.floorHeight);
-    this.context.fillStyle = '#283739';
-    this.context.fillRect(0, this.canvas.height-this.floorHeight, this.canvas.width, this.floorHeight);
-    }
     resize(width: number, height: number) {
+        height = Math.min(height, this.baseHeight);
+        
         this.canvas.width = width;
         this.canvas.height = height;
         this.context.fillStyle = 'red';
         this.width = this.canvas.width;
         this.height = this.canvas.height;
-        this.ratio = Number((this.height /this.baseHeight).toFixed(2));
+        this.ratio = Number((this.height / this.baseHeight).toFixed(2));
+        this.player.resize();
+        this.obstacle.resize();
     }
+    draw() {
+        // Draw ground
+    
+        this.context.fillStyle = '#f5f9ee';
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height-this.floorHeight);
+        this.context.fillStyle = '#283739';
+        this.context.fillRect(0, this.canvas.height-this.floorHeight, this.canvas.width, this.floorHeight);
+        }
     render() {
         this.draw();
 
