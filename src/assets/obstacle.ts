de48ae7b3387a4266004719e3bd6884d0a389041
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-04 18:08:57 +0000

    Updated obstacle.ts

--- src/assets/obstacle.ts
+++ src/assets/obstacle.ts
@@ -24,67 +24,74 @@ export default class Obstacle {
     }
     
     drawDarkTree(context: CanvasRenderingContext2D) {
+        const groundLevel1 = this.game.canvas.height - this.game.floorHeight - 80; 
+
         context.fillStyle = '#4c9173';
         context.beginPath();
-        context.arc(this.x - 16 + this.darkTreeXPosition + 20, this.y * 0.84, 16, 0, 2 * Math.PI);
-        context.arc(this.x - 18 + this.darkTreeXPosition + 20, this.y * 0.81, 16, 0, 2 * Math.PI);
-        context.arc(this.x - 7 + this.darkTreeXPosition + 20, this.y * 0.78, 16, 0, 2 * Math.PI);
-        context.arc(this.x - 2 + this.darkTreeXPosition + 20, this.y * 0.75, 16, 0, 2 * Math.PI);
-        context.arc(this.x + 10 + this.darkTreeXPosition + 20, this.y * 0.78, 16, 0, 2 * Math.PI);
-        context.arc(this.x + 16 + this.darkTreeXPosition + 20, this.y * 0.81, 16, 0, 2 * Math.PI);
-        context.arc(this.x + 16 + this.darkTreeXPosition + 20, this.y * 0.84, 16, 0, 2 * Math.PI);
+        context.arc(this.x - 16 + this.darkTreeXPosition + 20, groundLevel1 * 1.07, 17, 0, 2 * Math.PI);
+        context.arc(this.x - 18 + this.darkTreeXPosition + 20, groundLevel1 * 1.03, 17, 0, 2 * Math.PI);
+        context.arc(this.x - 7 + this.darkTreeXPosition + 20, groundLevel1 * 0.99, 17, 0, 2 * Math.PI);
+        context.arc(this.x - 2 + this.darkTreeXPosition + 20, groundLevel1 * 0.96, 17, 0, 2 * Math.PI);
+        context.arc(this.x + 10 + this.darkTreeXPosition + 20, groundLevel1 * 0.99, 17, 0, 2 * Math.PI);
+        context.arc(this.x + 16 + this.darkTreeXPosition + 20, groundLevel1 * 1.03, 17, 0, 2 * Math.PI);
+        context.arc(this.x + 16 + this.darkTreeXPosition + 20, groundLevel1 * 1.07, 17, 0, 2 * Math.PI);
         context.fill();
         context.fillStyle = '#402a23';
         context.beginPath();
-        context.arc(this.x - 0.55 + this.darkTreeXPosition + 20, this.y * 0.78, 5, 0, 2 * Math.PI);
+        context.arc(this.x - 0.55 + this.darkTreeXPosition + 20, groundLevel1, 5, 0, 2 * Math.PI);
         context.fill();
         context.fillStyle = '#402a23';
-        context.fillRect(this.x - 5.55 + this.darkTreeXPosition + 20, this.y * 0.78, 10, 80);
+        context.fillRect(this.x - 5.55 + this.darkTreeXPosition + 20, groundLevel1, 10, 80);
     }
 
     drawLightTreeDarkTree(context: CanvasRenderingContext2D) {
+        const groundLevel1 = this.game.canvas.height - this.game.floorHeight - 80; 
+        const groundLevel2 = this.game.canvas.height - this.game.floorHeight - 60; 
+
         context.fillStyle = '#acc6aa';
         context.beginPath();
-        context.arc(this.x - 16 + this.mixTreeXPosition + 400, this.y * 0.84, 16, 0, 2 * Math.PI);
-        context.arc(this.x - 18 + this.mixTreeXPosition + 400, this.y * 0.81, 16, 0, 2 * Math.PI);
-        context.arc(this.x - 7 + this.mixTreeXPosition + 400, this.y * 0.78, 16, 0, 2 * Math.PI);
-        context.arc(this.x - 2 + this.mixTreeXPosition + 400, this.y * 0.75, 16, 0, 2 * Math.PI);
-        context.arc(this.x + 10 + this.mixTreeXPosition + 400, this.y * 0.78, 16, 0, 2 * Math.PI);
-        context.arc(this.x + 16 + this.mixTreeXPosition + 400, this.y * 0.81, 16, 0, 2 * Math.PI);
-        context.arc(this.x + 16 + this.mixTreeXPosition + 400, this.y * 0.84, 16, 0, 2 * Math.PI);
+        context.arc(this.x - 16 + this.mixTreeXPosition + 400, groundLevel1 * 1.09, 17, 0, 2 * Math.PI);
+        context.arc(this.x - 18 + this.mixTreeXPosition + 400, groundLevel1 * 1.04, 17, 0, 2 * Math.PI);
+        context.arc(this.x - 7 + this.mixTreeXPosition + 400, groundLevel1 * 0.99, 17, 0, 2 * Math.PI);
+        context.arc(this.x - 2 + this.mixTreeXPosition + 400, groundLevel1 * 0.95, 17, 0, 2 * Math.PI);
+        context.arc(this.x + 10 + this.mixTreeXPosition + 400, groundLevel1 * 0.99, 17, 0, 2 * Math.PI);
+        context.arc(this.x + 16 + this.mixTreeXPosition + 400, groundLevel1 * 1.04, 17, 0, 2 * Math.PI);
+        context.arc(this.x + 16 + this.mixTreeXPosition + 400, groundLevel1 * 1.09, 17, 0, 2 * Math.PI);
         context.fill();
         context.fillStyle = '#402a23';
         context.beginPath();
-        context.arc(this.x - 0.55 + this.mixTreeXPosition + 400, this.y * 0.78, 5, 0, 2 * Math.PI);
+        context.arc(this.x - 0.55 + this.mixTreeXPosition + 400, groundLevel1, 5, 0, 2 * Math.PI);
         context.fill();
         context.fillStyle = '#402a23';
-        context.fillRect(this.x - 5.55 + this.mixTreeXPosition + 400, this.y * 0.78, 10, 80);
+        context.fillRect(this.x - 5.55 + this.mixTreeXPosition + 400, groundLevel1, 10, 80);
         context.fillStyle = '#4c9173';
         context.beginPath();
-        context.arc(this.x - 16 + this.mixTreeXPosition + 370, this.y * 0.82, 17, 0, 2 * Math.PI);
-        context.arc(this.x - 18 + this.mixTreeXPosition + 370, this.y * 0.80, 17, 0, 2 * Math.PI);
-        context.arc(this.x - 7 + this.mixTreeXPosition + 370, this.y * 0.78, 17, 0, 2 * Math.PI);
-        context.arc(this.x - 2 + this.mixTreeXPosition + 370, this.y * 0.76, 17, 0, 2 * Math.PI);
-        context.arc(this.x + 10 + this.mixTreeXPosition + 370, this.y * 0.78, 17, 0, 2 * Math.PI);
-        context.arc(this.x + 16 + this.mixTreeXPosition + 370, this.y * 0.80, 17, 0, 2 * Math.PI);
-        context.arc(this.x + 16 + this.mixTreeXPosition + 370, this.y * 0.82, 17, 0, 2 * Math.PI);
+        context.arc(this.x - 16 + this.mixTreeXPosition + 370, groundLevel2 * 1.02, 17, 0, 2 * Math.PI);
+        context.arc(this.x - 18 + this.mixTreeXPosition + 370, groundLevel2 * 0.98, 17, 0, 2 * Math.PI);
+        context.arc(this.x - 7 + this.mixTreeXPosition + 370, groundLevel2 * 0.97, 17, 0, 2 * Math.PI);
+        context.arc(this.x - 2 + this.mixTreeXPosition + 370, groundLevel2 * 0.94, 17, 0, 2 * Math.PI);
+        context.arc(this.x + 10 + this.mixTreeXPosition + 370, groundLevel2 * 0.97, 17, 0, 2 * Math.PI);
+        context.arc(this.x + 16 + this.mixTreeXPosition + 370, groundLevel2 * 0.98, 17, 0, 2 * Math.PI);
+        context.arc(this.x + 16 + this.mixTreeXPosition + 370, groundLevel2 * 1.02, 17, 0, 2 * Math.PI);
         context.fill();
         context.fillStyle = '#402a23';
         context.beginPath();
-        context.arc(this.x + 0.55 + this.mixTreeXPosition + 370, this.y * 0.8, 6, 0, 2 * Math.PI);
+        context.arc(this.x + 0.55 + this.mixTreeXPosition + 370, groundLevel2, 6, 0, 2 * Math.PI);
         context.fill();
         context.fillStyle = '#402a23';
-        context.fillRect(this.x - 5.55 + this.mixTreeXPosition + 370, this.y * 0.8, 12, 66);
+        context.fillRect(this.x - 5.55 + this.mixTreeXPosition + 370, groundLevel2, 12, 60);
     }
 
     drawStone(context: CanvasRenderingContext2D) {
+        const groundLevel = this.game.canvas.height - this.game.floorHeight; 
+
         context.fillStyle = '#000';
         const x1 = this.x - 11 + this.stoneXPosition + 30;
-        const y1 = this.y - 60;
+        const y1 = groundLevel - 60;
         const x2 = this.x - 42 + this.stoneXPosition + 30;
-        const y2 = this.y - 10;
+        const y2 = groundLevel;
         const x3 = this.x + this.stoneXPosition + 48;
-        const y3 = this.y - 10;
+        const y3 = groundLevel;
         context.beginPath();
         context.moveTo(x1, y1);
         context.lineTo(x2, y2)
@@ -94,28 +101,32 @@ export default class Obstacle {
     }
 
     drawCloudLight(context: CanvasRenderingContext2D) {
+        const groundLevel = (this.game.canvas.height - this.game.floorHeight) * 0.9; 
+
         context.fillStyle = '#bbe4e9';
         context.beginPath();
-        context.arc(this.x - 20 + this.cloudLightXPosition - 150, this.y * 0.27, 16, 0, 2 * Math.PI);
-        context.arc(this.x - 17 + this.cloudLightXPosition - 150, this.y * 0.24, 16, 0, 2 * Math.PI);
-        context.arc(this.x - 23 + this.cloudLightXPosition - 150, this.y * 0.26, 16, 0, 2 * Math.PI);
-        context.arc(this.x - 9 + this.cloudLightXPosition - 150, this.y * 0.26, 16, 0, 2 * Math.PI);
-        context.arc(this.x + this.cloudLightXPosition - 150, this.y * 0.25, 16, 0, 2 * Math.PI);
-        context.arc(this.x + 9 + this.cloudLightXPosition - 150, this.y * 0.27, 16, 0, 2 * Math.PI);
-        context.arc(this.x + 14 + this.cloudLightXPosition - 150, this.y * 0.27, 16, 0, 2 * Math.PI);
+        context.arc(this.x - 20 + this.cloudLightXPosition - 150, groundLevel * 0.27, 16, 0, 2 * Math.PI);
+        context.arc(this.x - 17 + this.cloudLightXPosition - 150, groundLevel * 0.24, 16, 0, 2 * Math.PI);
+        context.arc(this.x - 23 + this.cloudLightXPosition - 150, groundLevel * 0.26, 16, 0, 2 * Math.PI);
+        context.arc(this.x - 9 + this.cloudLightXPosition - 150, groundLevel * 0.26, 16, 0, 2 * Math.PI);
+        context.arc(this.x + this.cloudLightXPosition - 150, groundLevel * 0.25, 16, 0, 2 * Math.PI);
+        context.arc(this.x + 9 + this.cloudLightXPosition - 150, groundLevel * 0.27, 16, 0, 2 * Math.PI);
+        context.arc(this.x + 14 + this.cloudLightXPosition - 150, groundLevel * 0.27, 16, 0, 2 * Math.PI);
         context.fill();
     }
 
     drawCloudDark(context: CanvasRenderingContext2D) {
+        const groundLevel = (this.game.canvas.height - this.game.floorHeight) * 1.2; 
+        
         context.fillStyle = '#79c2d0';
         context.beginPath();
-        context.arc(this.x - 18 + this.cloudDarkXPosition - 50, this.y * 0.22, 14, 0, 2 * Math.PI);
-        context.arc(this.x - 15 + this.cloudDarkXPosition - 50, this.y * 0.23, 14, 0, 2 * Math.PI);
-        context.arc(this.x - 21 + this.cloudDarkXPosition - 50, this.y * 0.21, 14, 0, 2 * Math.PI);
-        context.arc(this.x - 7 + this.cloudDarkXPosition - 50, this.y * 0.23, 14, 0, 2 * Math.PI);
-        context.arc(this.x + this.cloudDarkXPosition - 50, this.y * 0.20, 14, 0, 2 * Math.PI);
-        context.arc(this.x + 7 + this.cloudDarkXPosition - 50, this.y * 0.22, 14, 0, 2 * Math.PI);
-        context.arc(this.x + 12 + this.cloudDarkXPosition - 50, this.y * 0.22, 14, 0, 2 * Math.PI);
+        context.arc(this.x - 18 + this.cloudDarkXPosition - 50, groundLevel * 0.22,  18, 0, 2 * Math.PI);
+        context.arc(this.x - 15 + this.cloudDarkXPosition - 50, groundLevel * 0.23,  18, 0, 2 * Math.PI);
+        context.arc(this.x - 21 + this.cloudDarkXPosition - 50, groundLevel * 0.21,  18, 0, 2 * Math.PI);
+        context.arc(this.x - 7 + this.cloudDarkXPosition - 50, groundLevel * 0.23,  18, 0, 2 * Math.PI);
+        context.arc(this.x + this.cloudDarkXPosition - 50, groundLevel * 0.20,  18, 0, 2 * Math.PI);
+        context.arc(this.x + 7 + this.cloudDarkXPosition - 50, groundLevel * 0.22,  18, 0, 2 * Math.PI);
+        context.arc(this.x + 12 + this.cloudDarkXPosition - 50, groundLevel * 0.22,  18, 0, 2 * Math.PI);
         context.fill();
     }
     updatePositionDarkTree() {
