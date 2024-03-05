4bdcd57e6109735ec46caf16907712e804e03e60
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-05 22:31:19 +0000

    Updated Game.vue logic - made jump logic in obstacle.ts

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -9,6 +9,9 @@
             @click="pauseGame">Pause</button>
         </div>
         <canvas ref="gameCanvas" @click="handleMouseEvent"></canvas>
+        <div class="assets">
+        <img src="../assets/images/background.png" alt="background" id="background">
+      </div>
     </div>
 </template>
 
@@ -18,84 +21,36 @@
 
     const gameCanvas = ref<HTMLCanvasElement | null>(null);
     const gameRunning = ref(false);
-    const jump = ref(false);
-    const doubleJump = ref(false);
-    let lastClickTime = 0;
-    const doubleJumpTimeout = 300;
     let animationFrameId: number | undefined;
 
     //game logic
 
-    const handleJump = () => {
-        if (!jump.value && !doubleJump.value) {
-            jump.value = true;
-            setTimeout(() => {
-                jump.value = false;
-            }, 6000);
-        } 
-    }
-
-    const handleDoubleJump = () => {
-        if (!doubleJump.value && !jump.value) {
-            doubleJump.value = true;
-            jump.value = false;
-            setTimeout(() => {
-                doubleJump.value = false;
-            }, 9000);
-        } 
-    }
-
-    const handleKeyDown = (event: KeyboardEvent) => {
-        const currentTime = new Date().getTime();
-            if (currentTime - lastClickTime < doubleJumpTimeout) {
-                handleDoubleJump();
-            } else {
-                handleJump();
-            }
-            lastClickTime = currentTime; 
-    }
-
-    // Define handleMouseEvent function
-    const handleMouseEvent = (event: MouseEvent) => {
-        if (gameCanvas.value && event.target === gameCanvas.value) {
-            const currentTime = new Date().getTime();
-            if (currentTime - lastClickTime < doubleJumpTimeout) {
-                handleDoubleJump();
-            } else {
-                handleJump();
-            }
-            lastClickTime = currentTime; 
-        }
-    }
-
     const startGame = () => {
-        jump.value = false;
-        doubleJump.value = false; 
         gameRunning.value = true;
         const context = gameCanvas.value?.getContext('2d');
         if (context && gameCanvas.value) {
             gameCanvas.value.width = 720;
             gameCanvas.value.height = 720;
-        
            
-            const game = new Game(gameCanvas.value, context, jump.value, doubleJump.value);
+            const game = new Game(gameCanvas.value, context);
             // Start animation loop
             
             const animate = () => {
+                if (gameCanvas.value.width) {
                 context.clearRect(0, 0, gameCanvas.value.width, gameCanvas.value.height);
+                }
                 game.render();
-                if (gameRunning.value) {
+                if (gameRunning.value && !game.gameOver) {
                     animationFrameId = requestAnimationFrame(animate);
                 }
             }
             animate();
         }
-        window.addEventListener('keydown', handleKeyDown);
     }
 
     const pauseGame = () => {
-        gameRunning.value = false;
         cancelAnimationFrame(animationFrameId);
+        gameRunning.value = false;
     }
 
 </script>
@@ -107,7 +62,7 @@
     };
 
     .buttonContainer {
-        background: $secondary-background;
+        background: none;
         @include flex(center, center, column);
         width: 140px;
         height: 100px;
@@ -115,8 +70,8 @@
         top: 0;
 
         button {
-            padding: 10px 25px;
-            font-size: 22px;
+            padding: 0 15px;
+            font-size: 18px;
             border-radius: 10px;
             background: $secondary-color;
             color: $primary-background;
