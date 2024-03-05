c1c8d4b0c030f1d815f176c614d2a645644472e3
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-05 22:31:40 +0000

    Updated player.ts

--- src/assets/player.ts
+++ src/assets/player.ts
@@ -4,59 +4,55 @@ export default class Player {
     game: Game;
     x: number;
     y: number;
-    radius: number;
-    color: string;
-    gameCanvasHeight: number;
+    width: number;
+    height: number;
+    spriteWidth: number;
+    spriteHeight: number;
+    speedY: number;
+    flapSpeed: number;
     
     constructor(game: Game) {
         this.game =  game;
-        this.x = this.game.canvas.width;
-        this.radius = 24;
-        this.y = this.game.canvas.height - this.game.floorHeight - this.radius;
-        this.color = '#5b086b';
-        this.gameCanvasHeight =this.game.canvas.height;
-        this.game =  game;
+        this.x = 20;
+        this.y = 0;
+        this.spriteWidth = 200;
+        this.spriteHeight = 200;
+        this.width = 0;
+        this.height = 0;
+        this.speedY = 0;
+        this.flapSpeed = 0;
     }
-    
-    draw(context: CanvasRenderingContext2D) {
-        const groundLevel = this.game.canvas.height - this.game.floorHeight - this.radius; 
-    
-        context.fillStyle = this.color;
-        context.beginPath();
-        this.game.context.arc(this.x, groundLevel, this.radius, 0, Math.PI * 2);
-        context.fill();
-        console.log(this.game.floorHeight);
-    }
-    
-    updatePosition(jump: boolean, doubleJump: boolean) {
-        console.log(jump)
-        // Update Y position
-        if (jump) {
-            this.y -= 2;
-            if (this.y < 90) {
-                this.y = 89;
-                setInterval(() => {
-                    this.y += 2;
-                    if (this.y >= 130) {
-                        this.y = 130;
-                    }
-                }, 100);
-            }  
-        } else if (doubleJump) {
-            this.y -= 3;
-            if (this.y < 70) {
-                this.y = 69;
-                setInterval(() => {
-                    this.y += 1.7;
-                    if (this.y >= 130) {
-                        this.y = 130;
-                    }
-                }, 100);
-            }
+    update() {
+        this.y += this.speedY;
+        if (!this.isTouchingBottom()) {
+            this.speedY += this.game.gravity;
         }
-    }
 
+        if (this.isTouchingBottom()) {
+            this.y = this.game.height - this.height;
+        }
+    } 
+    draw() {
+        this.game.context.fillRect(this.x, this.y, this.width, this.height);
+    }
     resize() {
-        this.radius = this.radius;
+        this.width = this.spriteWidth * this.game.ratio;
+        this.height = this.spriteHeight * this.game.ratio;
+        this.y = this.game.height * 0.5 - this.height * 0.5;
+        this.speedY = -8 * this.game.ratio;
+        this.flapSpeed = 5 * this.game.ratio;
+    }
+    isTouchingTop() {
+        return this.y <= 100;
+    }
+    isTouchingBottom() {
+        return this.y >= this.game.height - this.height;
+    }
+    flap() {
+        console.log( this.y)
+        if(!this.isTouchingTop()) {
+            this.speedY = -this.flapSpeed;
+        }
+      
     }
 }
