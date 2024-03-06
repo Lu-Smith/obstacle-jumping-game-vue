abcc6273fbfc5727618a0e269bfaaf8a2e36967e
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-06 16:10:09 +0000

    Added collision logic to obstacle.ts

--- src/assets/obstacle.ts
+++ src/assets/obstacle.ts
@@ -9,6 +9,9 @@ export default class Obstacle {
     x: number;
     y: number;
     markedForDeletion: boolean;
+    collisionX: number;
+    collisionY: number;
+    collisionRadius: number;
  
     constructor(game: Game, x: number) {
         this.game = game;
@@ -18,10 +21,15 @@ export default class Obstacle {
         this.scaledHeigth = this.spriteHeigth * this.game.ratio;
         this.x = x;
         this.y = 500 * this.game.ratio;
+        this.collisionX = 0;
+        this.collisionY = 0;
+        this.collisionRadius = this.scaledWidth * 0.5;
         this.markedForDeletion = false;
     }
     update() {
         this.x -= this.game.speed;
+        this.collisionX = this.x + this.scaledWidth * 0.5;
+        this.collisionY = this.y + this.scaledHeigth * 0.5;
     
         if (this.isOffScreen()) {
             this.markedForDeletion = true;
@@ -29,9 +37,17 @@ export default class Obstacle {
             this.game.score++;
             if (this.game.obstacles.length <= 0) this.game.gameOver = true;
         }
+        if (this.game.checkCollision(this, this.game.player)) {
+            this.game.gameOver = true;
+            this.game.player.collided = true;
+        }
     }
     draw(){
         this.game.context.fillRect(this.x, this.y, this.scaledWidth, this.scaledHeigth);
+        this.game.context.beginPath();
+        this.game.context.arc(this.collisionX, this.collisionY, 
+            this.collisionRadius, 0, Math.PI * 2);
+        this.game.context.stroke();
     }
     resize() {
         this.scaledWidth = this.spriteWidth * this.game.ratio;
