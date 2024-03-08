dfd40b0975c44981afcfca3f6f48147f2a2e3e19
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-08 17:38:07 +0000

    Updated player.ts

--- src/assets/player.ts
+++ src/assets/player.ts
@@ -9,11 +9,14 @@ export default class Player {
     spriteWidth: number;
     spriteHeight: number;
     speedY: number;
-    flapSpeed: number;
+    bounceSpeed: number;
     collisionX: number;
     collisionY: number;
     collisionRadius: number;
     collided: boolean;
+    image: CanvasImageSource;
+    collisionXStart: number;
+    frameY: number;
     
     constructor(game: Game) {
         this.game =  game;
@@ -24,11 +27,14 @@ export default class Player {
         this.width = 0;
         this.height = 0;
         this.speedY = 0;
-        this.flapSpeed = 0;
+        this.bounceSpeed = 0;
         this.collisionX = 0;
         this.collisionY = 0;
         this.collisionRadius = 0;
         this.collided = false;
+        this.image = document.getElementById('player') as CanvasImageSource;
+        this.collisionXStart = this.collisionX + this.collisionRadius * 0.8;
+        this.frameY = 0;
     }
     update() {
         this.y += this.speedY;
@@ -38,35 +44,54 @@ export default class Player {
         }
 
         if (this.isTouchingBottom()) {
-            this.y = this.game.height - this.height - 100 * this.game.ratio;
+            this.y = this.game.height - this.height - this.game.bottomMargin;
+            this.finsIdle();
         }
     } 
     draw() {
-        this.game.context.fillRect(this.x, this.y, this.width, this.height);
-        this.game.context.beginPath();
-        this.game.context.arc(this.collisionX, this.collisionY, 
-            this.collisionRadius, 0, Math.PI * 2);
-        this.game.context.stroke();
+        this.game.context.drawImage(this.image, 0, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, 
+            this.x, this.y, this.width, this.height);
+        if (this.game.debug) {
+            this.game.context.beginPath();
+            this.game.context.arc(this.collisionX, this.collisionY, 
+                this.collisionRadius, 0, Math.PI * 2);
+            this.game.context.stroke();
+        }
     }
     resize() {
         this.width = this.spriteWidth * this.game.ratio;
         this.height = this.spriteHeight * this.game.ratio;
         this.y = this.game.height * 0.5 - this.height * 0.5;
         this.speedY = -8 * this.game.ratio;
-        this.flapSpeed = 5 * this.game.ratio;
-        this.collisionRadius = this.width * 0.5;
+        this.bounceSpeed = 5 * this.game.ratio;
+        this.collisionRadius = this.width * 0.2;
         this.collisionX = this.x + this.width * 0.5;
         this.collided = false;
+        this.frameY = 0;
+    }
+    finsIdle() {
+        this.frameY = 0;
+    }
+    finsDown() {
+        this.frameY = 1;
+    }
+    finsUp() {
+        this.frameY = 2;
+    }
+    finsCharge() {
+        this.frameY = 3;
     }
     isTouchingTop() {
-        return this.y <= 100;
+        return this.y <= 200 * this.game.ratio;
     }
     isTouchingBottom() {
-        return this.y >= this.game.height - this.height - 100 * this.game.ratio;
+        return this.y >= this.game.height - this.height - this.game.bottomMargin;
     }
-    flap() {
+    bounce() {
         if(!this.isTouchingTop()) {
-            this.speedY = -this.flapSpeed;
+            // this.game.sound.play(this.game.sound.eatingSounds[0]);
+            this.speedY = -this.bounceSpeed;
+            this.finsDown()
         } 
     }
 }
