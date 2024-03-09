0d41591d264b94f7d67ba35f14ca92c2031bfa4f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-09 14:52:33 +0000

    Updated precision of collision in player.ts

--- src/assets/player.ts
+++ src/assets/player.ts
@@ -28,8 +28,8 @@ export default class Player {
         this.height = 0;
         this.speedY = 0;
         this.bounceSpeed = 0;
-        this.collisionX = 0;
-        this.collisionY = 0;
+        this.collisionX = 10;
+        this.collisionY = 5;
         this.collisionRadius = 0;
         this.collided = false;
         this.image = document.getElementById('player') as CanvasImageSource;
@@ -65,7 +65,7 @@ export default class Player {
         this.speedY = -8 * this.game.ratio;
         this.bounceSpeed = 5 * this.game.ratio;
         this.collisionRadius = this.width * 0.2;
-        this.collisionX = this.x + this.width * 0.5;
+        this.collisionX = this.x + this.width * 0.7;
         this.collided = false;
         this.frameY = 0;
     }
@@ -82,7 +82,7 @@ export default class Player {
         this.frameY = 3;
     }
     isTouchingTop() {
-        return this.y <= 20 * this.game.ratio;
+        return this.y <= 50 * this.game.ratio;
     }
     isTouchingBottom() {
         return this.y >= this.game.height - this.height - this.game.bottomMargin;
