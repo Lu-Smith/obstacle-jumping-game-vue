1d2543c11032f2f027b96434adca9d6a7d01fcfd
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-14 19:22:03 +0000

    Updated background.ts

--- src/assets/background.ts
+++ src/assets/background.ts
@@ -9,6 +9,7 @@ export default class Background {
     scaledWidth: number;
     scaledHeight: number;
     background: string;
+    nextBackgroundAlpha: number;
     
     constructor(game: Game) {
         this.game = game;
@@ -16,31 +17,33 @@ export default class Background {
         this.image = document.getElementById(this.background) as CanvasImageSource;
         this.width = 2400;
         this.height = this.game.baseHeight;
-       
         this.scaledWidth = this.width;
         this.scaledHeight = this.height;
         this.x = 0;
+        this.nextBackgroundAlpha = 0;
     }
     update(){
         this.x -= this.game.speed;
         if ( this.x <= - this.scaledWidth) this.x = 0;
-        if (this.game.score < 4 || this.game.score >= 8 && this.game.score < 12 || this.game.score >= 20 && this.game.score < 24) {
-            this.background = 'background2';
-        } else if (this.game.score >= 4 && this.game.score < 8 || this.game.score >= 12 && this.game.score < 20 || this.game.score >= 24 && this.game.score < 30) {
-            this.background = 'background1';
+        if (this.game.score < 48) {
+            if (this.game.level % 2 === 0 ) {
+                this.background = 'background2';
+            } else {
+                this.background = 'background1';
+            }
         } else {
             this.background = 'backgroundFinal';
             this.game.numberOfBirds = 0; 
             setTimeout(() => {
                 this.game.triggerGameOver();
-            }, 4000);
+            }, 1000);
         }
         this.image = document.getElementById(this.background) as CanvasImageSource; 
     }
     draw(){
-        this.game.context.drawImage(this.image, this.x, 0, this.scaledWidth, this.scaledHeight);
+        this.game.context.drawImage(this.image, this.x , 0, this.scaledWidth, this.scaledHeight);
         this.game.context.drawImage(this.image, this.x + this.scaledWidth - 1, 0, this.scaledWidth, this.scaledHeight);
-        if (this.game.canvas.width >= this.scaledWidth ) {
+        if (this.game.canvas.width >= this.scaledWidth) {
             this.game.context.drawImage(this.image, this.x + this.scaledWidth * 2 - 2, 0, this.scaledWidth, this.scaledHeight);
         }
     }
