d913162579cb411f47a3164c360e0f1822415c12
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-15 22:28:28 +0000

    Added new sound to audio.ts

--- src/assets/audio.ts
+++ src/assets/audio.ts
@@ -1,16 +1,20 @@
 export default class AudioControls {
     speed: HTMLAudioElement;
-    bounce: HTMLAudioElement;
+    bounce1: HTMLAudioElement;
+    bounce2: HTMLAudioElement;
     crunch: HTMLAudioElement;
     bite: HTMLAudioElement;
     eatingSounds: HTMLAudioElement[]; 
+    swimmingSounds: HTMLAudioElement[]; 
     win: HTMLAudioElement;
     lose: HTMLAudioElement;
     level: HTMLAudioElement;
 
     constructor() {
         this.speed = document.getElementById('speed') as HTMLAudioElement;
-        this.bounce = document.getElementById('bounce') as HTMLAudioElement;
+        this.bounce1 = document.getElementById('bounce1') as HTMLAudioElement;
+        this.bounce2 = document.getElementById('bounce2') as HTMLAudioElement;
+        this.swimmingSounds = [this.bounce1, this.bounce2];
         this.crunch = document.getElementById('crunch') as HTMLAudioElement;
         this.bite = document.getElementById('bite') as HTMLAudioElement;
         this.eatingSounds = [this.bite, this.crunch];
