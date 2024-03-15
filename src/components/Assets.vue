590464018d8e0f336112e36e13b6ed908d7b96db
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-15 18:54:00 +0000

    Created Assests.ts

---
+++ src/components/Assets.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="assets">
+        <img src="../assets/images/background1.png" alt="background" id="background1">
+        <img src="../assets/images/background2.png" alt="background" id="background2">
+        <img src="../assets/images/backgroundFinal.png" alt="final background" id="backgroundFinal">
+        <img src="../assets/images/player.png" alt="player" id="player">
+        <img src="../assets/images/monsters.png" alt="monsters" id="monsters">
+        <img src="../assets/images/monsters1.png" alt="monsters" id="monsters1">
+        <img src="../assets/images/key.png" alt="key" id="key">
+        <img src="../assets/images/heart.png" alt="heart" id="heart">
+        <img src="../assets/images/boubles2.png" alt="boubles" id="boubles2">
+        <audio src="../assets/media/speed.mp3" id="speed"></audio>
+        <audio src="../assets/media/crunch.mp3" id="crunch"></audio>
+        <audio src="../assets/media/bounce.mp3" id="bounce"></audio>
+        <audio src="../assets/media/win.mp3" id="win"></audio>
+        <audio src="../assets/media/lost.mp3" id="lose"></audio>
+    </div>
+</template>
