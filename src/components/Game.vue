bb64aa597e93d153f9b6d03c103719ba20fc3e06
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-15 18:54:28 +0000

    Added new components to Game.vue

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -6,33 +6,10 @@
         <button 
         v-if="gameRunning"
         @click="pauseGame">Pause</button>
-
-        <div v-if="!gameRunning && deltaTime === 0" class="gameDescription">
-            <h2><span>Tobby</span>'s Treasure Quest</h2>
-            <img src="../assets//images/fish.png" alt="fish">
-            <p>Tobby's Treasure Quest is an exhilarating underwater adventure where players take on the role of Tobby, a brave little fish on a mission to find hidden treasure. As Tobby swims through vibrant underwater environments, players must navigate through a maze of obstacles.</p>
-            <p>The main objective of the game is to collect keys scattered throughout each level while avoiding dangerous monsters and hazards. Players must use their quick reflexes and strategic thinking to outmaneuver the obstacles and progress through increasingly challenging levels.</p>
-            <p>Have an amazing adventure!</p>
-        </div>
-
+        <Description :gameRunning="gameRunning" :deltaTime="deltaTime"/>
         <Instructions :gameRunning="gameRunning" :showInfo="showInfo" @showInstructions="showInstructions" v-if="!gameRunning"/>
         <canvas ref="gameCanvas"></canvas>
-        <div class="assets">
-            <img src="../assets/images/background1.png" alt="background" id="background1">
-            <img src="../assets/images/background2.png" alt="background" id="background2">
-            <img src="../assets/images/backgroundFinal.png" alt="final background" id="backgroundFinal">
-            <img src="../assets/images/player.png" alt="player" id="player">
-            <img src="../assets/images/monsters.png" alt="monsters" id="monsters">
-            <img src="../assets/images/monsters1.png" alt="monsters" id="monsters1">
-            <img src="../assets/images/key.png" alt="key" id="key">
-            <img src="../assets/images/heart.png" alt="heart" id="heart">
-            <img src="../assets/images/boubles2.png" alt="boubles" id="boubles2">
-            <audio src="../assets/media/speed.mp3" id="speed"></audio>
-            <audio src="../assets/media/crunch.mp3" id="crunch"></audio>
-            <audio src="../assets/media/bounce.mp3" id="bounce"></audio>
-            <audio src="../assets/media/win.mp3" id="win"></audio>
-            <audio src="../assets/media/lost.mp3" id="lose"></audio>
-        </div>
+        <Assets />
     </div>
 </template>
 
@@ -40,6 +17,8 @@
     import { ref } from 'vue';
     import Game from '../assets/game.ts';
     import Instructions from './Instructions.vue';
+    import Description from './Description.vue';
+    import Assets from './Assets.vue';
 
     const gameCanvas = ref<HTMLCanvasElement | null>(null);
     const gameRunning = ref(false);
