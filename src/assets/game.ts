be2c7a9f728ce41aa2b971f1c1085ecb52e88a5f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-15 21:55:55 +0000

    Updated sound in game.ts

--- src/assets/game.ts
+++ src/assets/game.ts
@@ -245,11 +245,11 @@ export default class Game {
         if (!this.gameOver) { 
             this.gameOver = true; 
             if (this.score > 47 ) { 
-                // this.sound.play(this.sound.win);
+                this.sound.play(this.sound.win);
                 this.message1 = 'Nailed it - Your score is ' + this.score + '!';
                 this.message2 = 'Can you do it faster than ' +  this.formatTimer() + ' seconds?';
             } else {   
-                // this.sound.play(this.sound.lose);
+                this.sound.play(this.sound.lose);
                 this.message1 = 'Getting rusty?';
                 this.message2 = 'Collision time ' +  this.formatTimer() + ' seconds!';
             }
