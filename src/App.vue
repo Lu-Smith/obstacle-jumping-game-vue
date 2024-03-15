aaa6590ba409cd68527d02b12d2469a3037d8ee3
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-15 18:38:36 +0000

    Added Footer.vue to App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
     <div v-if="!startGame" class="mainPage">
       <IntroPage msg="Tobby's Treasure Quest" @startGame="handleStartGame"/>
     </div>
+    <div class="footerContainer" v-if="!startGame">
+      <Footer />
+    </div>
   </div>
 </template>
 
@@ -13,6 +16,7 @@
   import { ref } from 'vue';
   import Game from './components/Game.vue';
   import IntroPage from './components/IntroPage.vue';
+  import Footer from './components/Footer.vue';
 
   const startGame = ref(false);
 
@@ -33,6 +37,14 @@
       text-align: center;
       gap: 20px;
       padding-top: 10%;
+    }
+
+    .footerContainer {
+      position: fixed;
+      bottom: 0;
+      right: 0;
+      font-size: $primary-font-size;
+      line-height: $primary-line-height;
     }
   }
 
