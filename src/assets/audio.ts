cdadba52b7a055bd0cfdf4dd2070af025a1af748
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-03-15 22:11:37 +0000

    Updated audio.ts

--- src/assets/audio.ts
+++ src/assets/audio.ts
@@ -2,17 +2,19 @@ export default class AudioControls {
     speed: HTMLAudioElement;
     bounce: HTMLAudioElement;
     crunch: HTMLAudioElement;
+    bite: HTMLAudioElement;
     eatingSounds: HTMLAudioElement[]; 
     win: HTMLAudioElement;
     lose: HTMLAudioElement;
 
     constructor() {
-        this.speed = document.getElementById('speed') as HTMLAudioElement ;
-        this.bounce = document.getElementById('bounce') as HTMLAudioElement ;
-        this.crunch = document.getElementById('crunch') as HTMLAudioElement ;
-        this.eatingSounds = [this.speed, this.crunch];
-        this.win = document.getElementById('win') as HTMLAudioElement ;
-        this.lose = document.getElementById('lose') as HTMLAudioElement ;
+        this.speed = document.getElementById('speed') as HTMLAudioElement;
+        this.bounce = document.getElementById('bounce') as HTMLAudioElement;
+        this.crunch = document.getElementById('crunch') as HTMLAudioElement;
+        this.bite = document.getElementById('bite') as HTMLAudioElement;
+        this.eatingSounds = [this.bite, this.crunch];
+        this.win = document.getElementById('win') as HTMLAudioElement;
+        this.lose = document.getElementById('lose') as HTMLAudioElement;
     }
     play(sound: HTMLAudioElement ) {
         sound.currentTime = 0;
